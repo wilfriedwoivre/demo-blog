c1dc09fbe22a9523bc9ffa639956ab88256885fd
Wilfried Woivre <wilfried.woivre@gmail.com>
2017-12-22 18:08:25 +0100

    Add VNET creation article

---
+++ ARM/Tips VNET creation/create-demo-private-ip.ps1
@@ -0,0 +1,5 @@
+﻿$vnet = Get-AzureRmVirtualNetwork -Name 'demo-vnet' -ResourceGroupName 'demo-blog'
+$backSubnet = $vnet.Subnets | Where { $_.Name -eq 'Back' }
+
+$ipConf = New-AzureRmNetworkInterfaceIpConfig -Name 'demo-back-ipconf' -PrivateIpAddressVersion IPv4 -PrivateIpAddress '16.0.3.4' -Subnet $backSubnet
+New-AzureRmNetworkInterface -Name 'demo-back-ip' -ResourceGroupName 'demo-blog' -Location 'West Europe' -IpConfiguration $ipConf
